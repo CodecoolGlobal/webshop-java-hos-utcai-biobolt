3f21221d1b2f608d583fe84aa71a313994dba2e7
Jonatan Uri <urijoni@gmail.com>
2019-05-22 12:57:55 +0200

    Back-end filters products by category

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.ProductCategory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -31,9 +32,26 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(1));
+
+        int categoryId;
+        String categoryIdString = req.getParameter("categories");
+        if (categoryIdString == "all" || categoryIdString == null){
+            //IF ALL SELECTED (OR DEFAULT)
+            context.setVariable("category", productCategoryDataStore.find(1));
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        } else {
+            //IF CATEGORY SELECTED
+            try {
+                categoryId = Integer.parseInt(req.getParameter("categories"));
+                ProductCategory selectedCategory = productCategoryDataStore.find(categoryId);
+                context.setVariable("category", selectedCategory);
+                context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(categoryId)));
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }
         context.setVariable("categories", productCategoryDataStore.getAll());
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         engine.process("product/index.html", context, resp.getWriter());
     }
 
