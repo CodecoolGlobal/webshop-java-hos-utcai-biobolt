12989d9e996c22b8589bc2a13e7896601debfcf3
tejfi <adamfdeak@gmail.com>
2019-06-05 11:05:51 +0200

    getbycategory method in productDaoDB done

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoDB implements ProductDao {
@@ -18,6 +19,7 @@ public class ProductDaoDB implements ProductDao {
     ProductCategoryDB productCategoryDB;
     SupplierDaoDB supplierDaoDB;
     Product product;
+    List<Product> productList = new ArrayList<>();
 
     @Override
     public void add(Product product) {
@@ -66,6 +68,7 @@ public class ProductDaoDB implements ProductDao {
     @Override
     public void remove(int id) {
 
+
     }
 
     @Override
@@ -80,6 +83,28 @@ public class ProductDaoDB implements ProductDao {
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+        productList.clear();
+        String query = "SELECT * FROM products WHERE categoryid='" + productCategory.getId() + "';";
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+
+        ) {
+            while (resultSet.next()) {
+                Supplier supplier = supplierDaoDB.find(resultSet.getInt(resultSet.getInt("supplierid")));
+                productList.add(new Product(resultSet.getString("name"),
+                        Float.parseFloat(resultSet.getString("defaultprice")),
+                        resultSet.getString("currencystring"),
+                        resultSet.getString("description"),
+                        productCategory,
+                        supplier));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
     }
+
 }
+
